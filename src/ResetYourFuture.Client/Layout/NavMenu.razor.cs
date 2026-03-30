1531fe24b0ea9a4ff363aa9fa1843406da875098
s070s <gsotiro@hotmail.com>
2026-03-30 23:31:33 +0300

    Ui fixes

--- src/ResetYourFuture.Client/Layout/NavMenu.razor.cs
+++ src/ResetYourFuture.Client/Layout/NavMenu.razor.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
 namespace ResetYourFuture.Client.Layout;
 
 public partial class NavMenu
 {
+    [Parameter] public RenderFragment? TrailingContent { get; set; }
+
     private bool collapseNavMenu = true;
 
     private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
